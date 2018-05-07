b7049fa87f5f87d5547ad7d69a13de807ac12b5f
Ian Miell <ian.miell@gmail.com>
2018-05-07 06:24:14 +0100

    strace

--- telemetrise.py
+++ telemetrise.py
@@ -8,15 +8,17 @@ from curtsies.formatstring import linesplit
 
 
 def main(command):
-	# TODO: put in global object
+	# TODO: put in global object
 	command_session = pexpect.spawn(command)
 	command_pid = command_session.pid
-	pexpect.run('kill -STOP $PID')
+	pexpect.run('kill -STOP ' + str(command_pid))
 	# Assumes strace exists... need to correct/handle cases where not, eg mac
 	# or not installed. Also, what about root? TODO
-	strace_session = pexpect.spawn('strace -f -p $PID')
-	pexpect.run('kill -CONT $PID')
+	strace_session = pexpect.spawn('strace -f -p ' + str(command_pid))
+	pexpect.run('kill -CONT ' + str(command_pid))
 
+	command_output = ''
+	strace_output = ''
 	with curtsies.FullscreenWindow() as window:
 		while True:
 			# Setup
@@ -27,16 +29,32 @@ def main(command):
 			# Divide the screen up into two, to keep it simple for now
 			wheight_top_end    = int(wheight / 2)
 			wheight_bottom_start = int(wheight / 2) + 1
+			wwidth_left_end = int(wwidth / 2)
+			wwidth_right_start = int(wwidth / 2) + 1
 
 			# Header
 			header_text = 'telemetrise running on command: ' + command + ' ' + str(wheight) + 'x' + str(wwidth)
 			a[0:1,0:len(header_text)] = [blue(header_text)]
 
+			# Top half for command output
 			# Split the lines by newline, then reversed and zip up with line 2 to halfway.
 			if command_output != '':
+				# TODO: write wrap function to wrap lines that are too long before splitting
 				lines = command_output.split('\r\n')
 				for i, line in zip(reversed(range(2,wheight_top_end)), reversed(lines)):
-					a[i:i+1, 0:len(line)] = [line]
+					a[i:i+1, 0:len(line)] = [green(line)]
+
+			# Bottom left for strace output
+			if strace_output != '':
+				# TODO: write wrap function to wrap lines that are too long before splitting
+				lines = strace_output.split('\r\n')
+				for i, line in zip(reversed(range(wheight_bottom_start,wheight-2)), reversed(lines)):
+					line = line[:50]
+					a[i:i+1, 0:len(line)] = [red(line)]
+
+			# Footer
+			footer_text = 'telemetrise running on command: ' + command + ' ' + str(wheight) + 'x' + str(wwidth)
+			a[wheight-1:wheight,0:len(footer_text)] = [blue(footer_text)]
 
 			# We're done, now render!
 			#write_to_logfile(a)
@@ -46,28 +64,30 @@ def main(command):
 			char = None
 			# 'while' keeps it line-oriented for reasonable performance...
 			while char != '\n':
-				try:
-					char=command_session.read_nonblocking(timeout=0.1)
-				except pexpect.EOF:
-					command_output += 'EOF'
-				except:
-					command_output += '?'
-				if char:
-					command_output += char.decode('utf-8')
-				try:
-					char=strace_session.read_nonblocking(timeout=0.1)
-				except pexpect.EOF:
-					strace_output += 'EOF'
-				except:
-					strace_output += '?'
-				if char:
-					strace_output += char.decode('utf-8')
+				if command_session:
+					try:
+						char=command_session.read_nonblocking(timeout=1)
+					except pexpect.EOF:
+						command_output += '\nDONE'
+						command_session = None
+					except:
+						command_output += '?'
+					if char:
+						command_output += char.decode('utf-8')
+				if strace_session:
+					try:
+						char=strace_session.read_nonblocking(timeout=1)
+					except pexpect.EOF:
+						strace_output += '\nDONE'
+						strace_session = None
+					except:
+						strace_output += '?'
+					if char:
+						strace_output += char.decode('utf-8')
 
 
 # TODO: object for each session
 command = 'ping -c100 google.com'
-command_output = ''
-strace_output = ''
 logfile = open('outfile','w')
 
 
@@ -75,6 +95,7 @@ def write_to_logfile(msg):
 	global logfile
 	logfile.write(str(msg) + '\n')
 	logfile.flush()
+
 
 if __name__ == '__main__':
 	main(command)
