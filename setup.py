5a168ccc241e19c891dc04bb53a790a7606cf18e
Ian Miell <ian.miell@gmail.com>
2018-05-08 20:38:17 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,16 +2,9 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
-
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
 setup(
     name='telemetrise',
  version='0.0.5',
