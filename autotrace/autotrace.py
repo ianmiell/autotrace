6f8f8b898d29dce04dd5b369c6b98ae10dd20cc1
Ian Miell <ian.miell@gmail.com>
2018-05-13 09:16:49 +0100

    scrolling now working

--- autotrace/autotrace.py
+++ autotrace/autotrace.py
@@ -337,6 +337,10 @@ class PexpectSessionManager(object):
 					session.output_top_visible_line_index = None
 				else:
 					return_msg = ' at least one session has hit the top'
+				self.debug_msg('================================================================================')
+				self.debug_msg('scrolling backwards in session: ' + str(session.pid))
+				self.debug_msg(str(self))
+				self.debug_msg(str(session))
 		return return_msg
 
 	def scroll_forward(self):
@@ -350,8 +354,10 @@ class PexpectSessionManager(object):
 					session.output_lines_end_pane_pointer = None
 				else:
 					return_msg = ' at least one session has hit the end' 
-				self.debug_msg('scrolling forward')
+				self.debug_msg('================================================================================')
+				self.debug_msg('scrolling forward in session: ' + str(session.pid))
 				self.debug_msg(str(self))
+				self.debug_msg(str(session))
 		return return_msg
 
 	def move_panes_to_tail(self):
@@ -391,14 +397,17 @@ class PexpectSession(object):
 		string += '\nsession_number: ' + str(self.session_number)
 		string += '\ncommand: ' + str(self.command)
 		string += '\npid: ' + str(self.pid)
-		string += '\noutput_lines length: ' + str(len(self.output_lines_end_pane_pointer))
-		string += '\noutput_lines_end_pane_pointer: ' + str(self.output_lines_end_pane_pointer)
-		string += '\noutput_top_visible_line_index: ' + str(self.output_top_visible_line_index)
+		string += '\noutput_lines length: ' + str(len(self.output_lines))
+		if self.output_lines_end_pane_pointer is not None:
+			string += '\noutput_lines_end_pane_pointer: ' + str(self.output_lines_end_pane_pointer)
+		if self.output_top_visible_line_index is not None:
+			string += '\noutput_top_visible_line_index: ' + str(self.output_top_visible_line_index)
 		return string
 
 	def write_out_session_to_fit_pane(self):
 		"""This function is responsible for taking the state of the session and writing it out to its pane.
 		"""
+		self.pexpect_session_manager.debug_msg('In write_out_session_to_fit_pane for session: ' + str(self.pid))
 		pane = self.session_pane
 		if pane:
 			assert self.session_pane
@@ -432,19 +441,27 @@ class PexpectSession(object):
 					last_time_seen = int(line_obj.time_seen)
 				# Strip whitespace at end, including \r\n
 				line = line_obj.line_str.rstrip()
+				if pane_line_counter is None and self.output_top_visible_line_index == output_lines_cursor:
+					# We are within the realm of the pane now
+					pane_line_counter = 0
 				while len(line) > width-1:
 					# When we get to the top visible line index, kick off the
 					# counter and up one for each pane line computed.
-					if pane_line_counter is None and self.output_top_visible_line_index == output_lines_cursor:
-						# We are within the realm of the pane now
-						pane_line_counter = 0
+					self.pexpect_session_manager.debug_msg('pane_line_counter = ' + str(pane_line_counter))
+					self.pexpect_session_manager.debug_msg('height = ' + str(height))
 					lines_in_pane_str_arr.append([line[:width-1], output_lines_cursor])
 					line = line[width-1:]
 					if pane_line_counter is not None:
+						self.pexpect_session_manager.debug_msg(line)
 						pane_line_counter += 1
 						if pane_line_counter > height - 1:
 							break
 				lines_in_pane_str_arr.append([line, output_lines_cursor])
+				if pane_line_counter is not None:
+					self.pexpect_session_manager.debug_msg(line)
+					pane_line_counter += 1
+					if pane_line_counter > height - 1:
+						break
 			output_lines_end_pane_pointer_has_been_set = False
 			for i, line in zip(reversed(range(pane.top_left_y,pane.bottom_right_y)), reversed(lines_in_pane_str_arr)):
 				self.pexpect_session_manager.screen_arr[i:i+1, pane.top_left_x:pane.top_left_x+len(line[0])] = [pane.color(line[0])]
@@ -494,6 +511,9 @@ class PexpectSession(object):
 			self.output_lines_end_pane_pointer = len(self.output_lines)-1
 		else:
 			self.output_lines_end_pane_pointer += 1
+		# Move pane visibility along one too.
+		if self.output_top_visible_line_index is not None:
+			self.output_top_visible_line_index += 1
 
 
 # Represents a line in the array of output
